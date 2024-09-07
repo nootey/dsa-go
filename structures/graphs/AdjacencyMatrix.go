82de87f819db888d25e7b18f4b74a25bb8a363a8
Nootey33 <poiskusni@gmail.com>
2024-09-07 09:33:31 +0200

    expose internals

--- structures/graphs/AdjacencyMatrix.go
+++ structures/graphs/AdjacencyMatrix.go
@@ -3,12 +3,12 @@ package graphs
 import "fmt"
 
 type Node struct {
-	data string
+	Data string
 }
 
 type AdjacencyMatrixGraph struct {
-	matrix [][]int
-	nodes  []Node
+	Matrix [][]int
+	Nodes  []Node
 }
 
 // CreateNewGraph creates a new graph with the specified size (number of vertices)
@@ -17,28 +17,28 @@ func CreateNewGraph(size int) *AdjacencyMatrixGraph {
 	for i := range matrix {
 		matrix[i] = make([]int, size) // Initialize each row
 	}
-	return &AdjacencyMatrixGraph{matrix: matrix}
+	return &AdjacencyMatrixGraph{Matrix: matrix}
 }
 
 // AddNode adds a new node to the graph
 func (g *AdjacencyMatrixGraph) AddNode(data string) {
-	node := Node{data: data}
-	g.nodes = append(g.nodes, node)
+	node := Node{Data: data}
+	g.Nodes = append(g.Nodes, node)
 }
 
 // AddEdge adds a directed edge between two vertices
 func (g *AdjacencyMatrixGraph) AddEdge(src int, dest int) {
 	// Ensure that src and dest are within the bounds of the graph size
-	if src < len(g.matrix) && dest < len(g.matrix) {
-		g.matrix[src][dest] = 1
+	if src < len(g.Matrix) && dest < len(g.Matrix) {
+		g.Matrix[src][dest] = 1
 	}
 }
 
 // CheckEdge checks if there is an edge between two vertices
 func (g *AdjacencyMatrixGraph) CheckEdge(src int, dest int) bool {
 	// Ensure that src and dest are within the bounds of the graph size
-	if src < len(g.matrix) && dest < len(g.matrix) {
-		return g.matrix[src][dest] == 1
+	if src < len(g.Matrix) && dest < len(g.Matrix) {
+		return g.Matrix[src][dest] == 1
 	}
 	return false
 }
@@ -46,7 +46,7 @@ func (g *AdjacencyMatrixGraph) CheckEdge(src int, dest int) bool {
 // PrintMatrix prints the adjacency matrix of the graph
 func (g *AdjacencyMatrixGraph) PrintMatrix() {
 	fmt.Println("Adjacency Matrix:")
-	for _, row := range g.matrix {
+	for _, row := range g.Matrix {
 		fmt.Println(row)
 	}
 }
